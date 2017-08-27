c19ad16d220bdd6b27f321b75af37dcf72331481
kakas <shrimptrain@gmail.com>
2017-08-27 23:04:52 +0800

    implement my account/posts#edit, update, destroy

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -4,4 +4,29 @@ class Account::PostsController < ApplicationController
   def index
     @posts = current_user.posts
   end
+
+  def edit
+    @post = current_user.posts.find(params[:id])
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to account_posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destory
+    @post = current_user.posts.find(params[:id])
+    @post.destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
